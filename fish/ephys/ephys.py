2d5f0cfcb72743084734920d1e71451a61529a77
d-v-b <vaslav.tchitcherine@gmail.com>
2016-08-27 16:19:52 -0400

    refactored and simplified ephys.load, ephys.windowed_variance

--- fish/ephys/ephys.py
+++ fish/ephys/ephys.py
@@ -1,21 +1,21 @@
 """Process electrophysiological recordings of fish behavior and trial structure"""
 
-import numpy as np 
 
 def chopTrials(signal, trialThr=2000):
     """for each unique value in the signal, 
        return the start and stop of each epoch corresponding to that value
     """
-    
-    allCond = np.unique(signal)        
+    from numpy import unique, where, concatenate
+
+    conditions = unique(signal)
     chopped = {}
-    for c in allCond:
-        tmp = np.where(signal == c)[0]
-        offs = np.where(np.diff(tmp) > 1)[0]
-        offs = np.concatenate((offs, [tmp.size-1]))
-        ons = np.concatenate(([0], offs[0:-1] + 1))
+    for c in conditions:
+        tmp = where(signal == c)[0]
+        offs = where(np.diff(tmp) > 1)[0]
+        offs = concatenate((offs, [tmp.size-1]))
+        ons = concatenate(([0], offs[0:-1] + 1))
         trLens = offs - ons        
-        keepTrials = np.where(trLens > trialThr)
+        keepTrials = where(trLens > trialThr)
         offs = offs[keepTrials]
         ons = ons[keepTrials]
         chopped[c] = (tmp[ons], tmp[offs])
@@ -23,50 +23,60 @@ def chopTrials(signal, trialThr=2000):
     return chopped
 
 
-def stackInits(frameCh,thrMag=3.8,thrDur=10):    
+def stack_inits(signal, thr=3.8, dur=10):
     """
         Find indices in ephys time corresponding to the onset of each stack in image time
     """
+
+    from numpy import where, diff, concatenate
+
+    # find indices where the signal is above threshold
+    inits = where(signal > thr)[0]
+
+    # find indices where the threshold crossings are non-consecutive
+    init_diffs = where(diff(inits) > 1)[0]
+
+    # add a 0 to count the first threshold crossing
+    init_diffs = concatenate(([0], init_diffs+1))
+
+    # index the threshold crossing indices with the non-consecutive indices
+    inits = inits[init_diffs]
+
+    keepers = concatenate((where(diff(inits) > dur)[0], [inits.size-1]))
+    inits = inits[keepers]
     
-    stackInits = np.where(frameCh > thrMag)[0]
-    initDiffs = np.where(np.diff(stackInits) > 1)[0]
-    initDiffs = np.concatenate(([0], initDiffs+1))    
-    stackInits = stackInits[initDiffs]
-    keepers = np.concatenate((np.where(np.diff(stackInits) > thrDur)[0], [stackInits.size-1]))
-    stackInits = stackInits[keepers]
-    
-    return stackInits
+    return inits
 
 
 def getSwims(ch):
     """ Estimate swim timing from ephys recording of motor neurons
     """
     
-    from scipy import signal as sig
+    from numpy import zeros, where, diff, concatenate
 
     # set dead time, in samples
     deadT = 80
     
-    fltch = smoothPower(ch)
+    fltch = windowed_variance(ch)
     peaksT, peaksIndT = getPeaks(fltch, deadT)
-    thr = getThreshold(fltch,2600000)
-    burstIndT = peaksIndT[np.where(fltch[peaksIndT] > thr[peaksIndT])]
-    burstT = np.zeros(fltch.shape)
+    thr = getThreshold(fltch, 2600000)
+    burstIndT = peaksIndT[where(fltch[peaksIndT] > thr[peaksIndT])]
+    burstT = zeros(fltch.shape)
     burstT[burstIndT] = 1
     
-    interSwims = np.diff(burstIndT)
-    swimEndIndB = np.where(interSwims > 800)[0]
-    swimEndIndB = np.concatenate((swimEndIndB,[burstIndT.size-1]))
+    interSwims = diff(burstIndT)
+    swimEndIndB = where(interSwims > 800)[0]
+    swimEndIndB = concatenate((swimEndIndB, [burstIndT.size-1]))
 
     swimStartIndB = swimEndIndB[0:-1] + 1
-    swimStartIndB = np.concatenate(([0], swimStartIndB))
-    nonShort = np.where(swimEndIndB != swimStartIndB)[0]
+    swimStartIndB = concatenate(([0], swimStartIndB))
+    nonShort = where(swimEndIndB != swimStartIndB)[0]
     swimStartIndB = swimStartIndB[nonShort]
     swimEndIndB = swimEndIndB[nonShort]
 
-    bursts = np.zeros(fltch.size)
-    starts = np.zeros(fltch.size)
-    stops = np.zeros(fltch.size)
+    bursts = zeros(fltch.size)
+    starts = zeros(fltch.size)
+    stops = zeros(fltch.size)
     bursts[burstIndT] = 1
     starts[burstIndT[swimStartIndB]] = 1
     stops[burstIndT[swimEndIndB]] = 1
@@ -75,26 +85,31 @@ def getSwims(ch):
 
 
 # filter signal, extract power
-def smoothPower(ch, kern=None):
+def windowed_variance(signal, kern_mean=None, kern_var=None):
     """
-    subtract smoothed vector from raw vector and square to estimate swim power
-    :param ch:
-    :param kern:
-    :return:
+    Estimate sliding variance of the input signal
+    :param signal:
+    :param kern_mean:
+    :return kern_var:
     """
-    from scipy import signal as sig
+    from scipy.signal import gaussian, fftconvolve
 
-    if kern is None:
-        kern = sig.gaussian(121, 20)
-        kern = kern/kern.sum()
+    if kern_mean is None:
+        kern_mean = gaussian(121, 20)
+        kern_mean /= kern_mean.sum()
 
-    smch = sig.fftconvolve(ch, kern, 'same')
-    power = (ch - smch)**2
-    fltch = sig.fftconvolve(power, kern, 'same')
+    if kern_var is None:
+        kern_var = gaussian(121, 20)
+        kern_var /= kern_var.sum()
+
+    mean_estimate = fftconvolve(signal, kern_mean, 'same')
+    power = (signal - mean_estimate)**2
+    fltch = fftconvolve(power, kern_var, 'same')
     return fltch
 
+
 # get peaks
-def getPeaks(fltch,deadTime=80):
+def getPeaks(fltch, deadTime=80):
     
     aa = np.diff(fltch)
     peaks = (aa[0:-1] > 0) * (aa[1:] < 0)
@@ -113,14 +128,15 @@ def getPeaks(fltch,deadTime=80):
     peaks = np.zeros(fltch.size)
     peaks[inds] = 1
     
-    return peaks,inds
+    return peaks, inds
+
 
 # find threshold
-def getThreshold(fltch,wind=180000,shiftScale=1.6):
+def getThreshold(fltch, wind=180000, shiftScale=1.6):
     
     th = np.zeros(fltch.shape)
     
-    for t in np.arange(0,fltch.size-wind, wind):
+    for t in np.arange(0, fltch.size-wind, wind):
 
         interval = np.arange(t, t+wind)
         sqrFltch = fltch ** .5            
@@ -131,16 +147,19 @@ def getThreshold(fltch,wind=180000,shiftScale=1.6):
 
     return th
 
-def load(inFile):
+
+def load(in_file):
     """Load 10chFlt data from disk, return as a [channels,samples] sized numpy array
     """
-    fd = open(inFile, 'rb')
-    data = np.fromfile(file=fd, dtype=np.float32)
-    trim = len(data) % 10
-    if trim == 0:
-        data = data.reshape(len(data)//10, 10).T
-    else:
-        data = data[:-trim]
-        data = data.reshape(len(data) // 10, 10).T
+    from numpy import fromfile, float32
+
+    fd = open(in_file, 'rb')
+    data = fromfile(file=fd, dtype=float32)
+    n_chan = 10
+    trim = len(data) % n_chan
+    # transpose to make dimensions [channels, time]
+    data = data[:(data.size - trim)].reshape(data.size // n_chan, n_chan).T
+    if trim > 0:
         print('Data needed to be truncated!')
+
     return data
