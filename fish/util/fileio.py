6d5500e3968bc3b5ba3e7f352536c322914aeac0
Bennett <bennettd@bennettd-lm1.local>
2017-05-26 15:32:37 -0400

    image_conversion now uses load_image

--- fish/util/fileio.py
+++ fish/util/fileio.py
@@ -29,7 +29,7 @@ def load_image(fname):
         from h5py import File
         with File(h5_path, 'r') as f:
             return f['default'].value
-
+    # Get the file extension for this file, assuming it is the last continuous string after the last period
     fmt = fname.split('.')[-1]
 
     loaders = dict()
@@ -57,29 +57,12 @@ def image_conversion(source_path, dest_fmt, wipe=False):
     from os import remove
 
     source_name = source_path.split('.')[0]
-    source_fmt = source_path.split('.')[-1]
     dest_path = source_name + '.' + dest_fmt
 
-    def stack_loader(stack_path):
-        from fish.image import vol as volt
-        from numpy import fromfile
-        from os.path import sep, split
-        dims = volt.get_stack_dims(split(stack_path)[0] + sep)
-        im = fromfile(stack_path, dtype='int16')
-        im = im.reshape(dims[-1::-1])
-        return im
-
-    def tif_loader(tif_path):
-        from skimage.io import imread
-        return imread(tif_path)
 
     def klb_writer(data, klb_path):
         from pyklb import writefull
         writefull(data, klb_path)
-
-    def klb_loader(klb_path):
-        from pyklb import readfull
-        return readfull(klb_path)
 
     def h5_writer(data, h5_path):
         from h5py import File
@@ -92,26 +75,13 @@ def image_conversion(source_path, dest_fmt, wipe=False):
         f.create_dataset('default', data=data, compression='gzip', chunks=True, shuffle=True)
         f.close()
 
-    def h5_loader(h5_path):
-        from h5py import File
-        with File(h5_path, 'r') as f:
-            return f['default'].value
-
-    if source_fmt == 'stack':
-        source_loader = stack_loader
-
-    elif source_fmt == 'tif':
-        source_loader = tif_loader
-
     if dest_fmt == 'klb':
         dest_writer = klb_writer
-        dest_reader = klb_reader
 
     elif dest_fmt == 'h5':
         dest_writer = h5_writer
-        dest_reader = h5_reader
 
-    source_image = source_loader(source_path)
+    source_image = load_image(source_name)
     dest_writer(source_image, dest_path)
 
     if wipe:
