bd11087f74ddf1690faaffc90bd65c3164c9c990
d-v-b <davis.v.bennett@gmail.com>
2018-04-29 15:31:54 -0400

    updated dask_drmaa wrapper to no longer dump logfiles to the pwd

--- fish/util/distributed.py
+++ fish/util/distributed.py
@@ -12,20 +12,32 @@
 
 
 def get_cluster():
+    """
+    Instatiate a DRMAACluster for use with the LSF scheduler on the Janelia Research Campus compute cluster. This is a
+    wrapper for dask_drmaa.DRMMACluster that uses reasonable default settings for the dask workers. Specifically, this
+    ensures that dask workers use the /scratch/$USER directory for temporary files and also that each worker runs on a
+    single core. This wrapper also directs the $WORKER.err and $WORKER.log files to /scratch/$USER.
+
+    """
     from dask_drmaa import DRMAACluster
     import os
-    # todo: make these arguments function properly
-    output_path = '/groups/ahrens/home/bennettd/dask_tmp/'
+
+    local_directory = '/scratch/' + os.environ['USER']
+    output_path = ':' + local_directory
     error_path = output_path
     cluster_kwargs_pass = {}
     cluster_kwargs_pass.setdefault(
-            'template',
-            {
-                'args': [
+        'template',
+        {
+            'args': [
                 '--nthreads', '1',
-                '--local-directory', '/scratch/' + os.environ['USER']],
-                'jobEnvironment': os.environ
-            }
-        )
-    cluster = DRMAACluster(**cluster_kwargs_pass, errorPath=error_path, outputPath=output_path)
+                '--local-directory', local_directory],
+            'jobEnvironment': os.environ,
+            'outputPath': output_path,
+            'errorPath': error_path
+
+        }
+    )
+    cluster = DRMAACluster(**cluster_kwargs_pass)
     return cluster
+
