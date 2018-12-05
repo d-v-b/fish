f55cdddc780f6e4e0c8cf5c4b057d8cf26fa0566
d-v-b <davis.v.bennett@gmail.com>
2018-12-05 18:47:05 -0500

    added downsampled_baseline function

--- fish/util/distributed.py
+++ fish/util/distributed.py
@@ -66,3 +66,43 @@ def get_drmaa_cluster():
     cluster = DRMAACluster(**cluster_kwargs_pass)
     return cluster
 
+
+def get_downsampled_baseline(keyframes, data, axis=0, perc=50, window=301):
+    """
+    Generate a dask array that will take the non-sliding windowed percentile of input data along the first axis.
+    Note that there is no handling of edges. Values of keyframes at the edges of the range will be result in
+    the percentile being estimated over fewer values.
+
+    returns a stacked dask array where each element is the lazy percentile estimated over the 0th...len(keyframes)th
+    window. The dtype of this array will be float32.
+
+    keyframes : numpy array, timepoints at which to take the windowed percentile.
+
+    data : dask array
+
+    axis : integer, axis along which to take the percentile. defaults to 0.
+
+    perc : integer between 0 and 100. This is the percentile that will be measured in each window.
+
+    window : integer, size of the window to use for computing the percentile.
+
+    """
+
+    from numpy import arange, array, percentile, clip
+    from dask.array import stack
+
+    inds = array(
+        [arange(clip(k - window // 2, 0, None), clip(k + window // 2, 0, data.shape[axis] - 1)) for k in keyframes])
+
+    def get_perc(v):
+        return percentile(v, perc, axis=axis).astype('float32')
+    rechunked = []
+    for i in inds:
+        new_chunks = ['auto'] * data.ndim
+        new_chunks[axis] = len(i)
+        new_chunks = tuple(new_chunks)
+        rechunked.append(data[i].rechunk(new_chunks))
+
+    stacked = stack([r.map_blocks(get_perc, dtype='float32', drop_axis=axis) for r in rechunked])
+
+    return stacked
